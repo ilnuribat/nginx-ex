d97ee79e36ca76197666ec37f0bce1064fb01075
ilnuribat <ilnuribat@gmail.com>
2018-04-29 02:39:38 +0300

    changes name of dir

--- start.sh
+++ start.sh
@@ -5,9 +5,9 @@ docker stop nginx-ex || true
 docker rm nginx-ex || true
 
 docker run \
-	-v ~/projects/nginx/nginx.conf:/etc/nginx/nginx.conf \
-	-v ~/projects/nginx/htmls:/data/htmls \
-	-v ~/projects/nginx/images:/data/images \
+	-v ~/projects/nginx-ex/nginx.conf:/etc/nginx/nginx.conf \
+	-v ~/projects/nginx-ex/htmls:/data/htmls \
+	-v ~/projects/nginx-ex/images:/data/images \
 	-p 80:80 \
 	--name=nginx-ex \
 	nginx:1.13
