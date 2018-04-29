df6b2da83101b0ed5dfa26e85536d5903edccf41
ilnuribat <ilnuribat@gmail.com>
2018-04-29 19:25:11 +0300

    added certs, give port

--- start.sh
+++ start.sh
@@ -8,6 +8,8 @@ docker run \
 	-v ~/projects/nginx-ex/nginx.conf:/etc/nginx/nginx.conf \
 	-v ~/projects/nginx-ex/htmls:/data/htmls \
 	-v ~/projects/nginx-ex/images:/data/images \
+	-v ~/projects/nginx-ex/certs/:/data/certs/
 	-p 80:80 \
+	-p 443:443 \
 	--name=nginx-ex \
 	nginx:1.13
